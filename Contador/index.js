a0d77442484c36396808ea8e7ec0ff821817a1f3
Sandson <Sandson.contato@gmail.com>
2023-01-07 23:28:48 -0300

    07/01/2023
    -Interatividade completa
    -Estilo finalizado

---
+++ Contador/index.js
@@ -0,0 +1,27 @@
+var valorMaleavel = 0
+function executando(condicao){
+    let numeroDiplay = document.getElementById("valor")
+    if(condicao == 0){
+        numeroDiplay.innerHTML = valorMaleavel += 1
+        if(valorMaleavel%10 == 0){
+            numeroDiplay.style.color ="red"
+        }
+        else{
+            numeroDiplay.style.color ="black"
+        }
+    }
+    else if (condicao == 1 && valorMaleavel > 0){
+        numeroDiplay.innerHTML = valorMaleavel -= 1
+        if(valorMaleavel%10 == 0){
+            numeroDiplay.style.color ="red"
+        }
+        else{
+            numeroDiplay.style.color ="black"
+        }
+    }
+}
+function limpar(){
+    valorMaleavel = 0
+    let numeroDiplay = document.getElementById("valor")
+    numeroDiplay.innerHTML = valorMaleavel
+}
